4aa5713f37d84aa7f0b10c85a63e737f4b2185e0
Robert <rgonser@csustan.edu>
2016-05-18 00:13:45 -0700

    Add project files.

---
+++ ArrayVisualization/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ArrayVisualization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawingArea = new System.Windows.Forms.PictureBox();
+            this.insertSortBtn = new System.Windows.Forms.Button();
+            this.drawArray = new System.Windows.Forms.Button();
+            this.shuffleArrayBtn = new System.Windows.Forms.Button();
+            this.selectSortBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.drawingArea)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawingArea
+            // 
+            this.drawingArea.Location = new System.Drawing.Point(139, 12);
+            this.drawingArea.Name = "drawingArea";
+            this.drawingArea.Size = new System.Drawing.Size(793, 478);
+            this.drawingArea.TabIndex = 0;
+            this.drawingArea.TabStop = false;
+            // 
+            // insertSortBtn
+            // 
+            this.insertSortBtn.Location = new System.Drawing.Point(12, 150);
+            this.insertSortBtn.Name = "insertSortBtn";
+            this.insertSortBtn.Size = new System.Drawing.Size(120, 63);
+            this.insertSortBtn.TabIndex = 1;
+            this.insertSortBtn.Text = "Insertion Sort!";
+            this.insertSortBtn.UseVisualStyleBackColor = true;
+            this.insertSortBtn.Click += new System.EventHandler(this.insertSortBtn_Click);
+            // 
+            // drawArray
+            // 
+            this.drawArray.Location = new System.Drawing.Point(12, 12);
+            this.drawArray.Name = "drawArray";
+            this.drawArray.Size = new System.Drawing.Size(121, 63);
+            this.drawArray.TabIndex = 2;
+            this.drawArray.Text = "Draw Array!";
+            this.drawArray.UseVisualStyleBackColor = true;
+            this.drawArray.Click += new System.EventHandler(this.drawArray_Click);
+            // 
+            // shuffleArrayBtn
+            // 
+            this.shuffleArrayBtn.Location = new System.Drawing.Point(12, 81);
+            this.shuffleArrayBtn.Name = "shuffleArrayBtn";
+            this.shuffleArrayBtn.Size = new System.Drawing.Size(121, 63);
+            this.shuffleArrayBtn.TabIndex = 3;
+            this.shuffleArrayBtn.Text = "Shuffle Array!";
+            this.shuffleArrayBtn.UseVisualStyleBackColor = true;
+            this.shuffleArrayBtn.Click += new System.EventHandler(this.shuffleArrayBtn_Click);
+            // 
+            // selectSortBtn
+            // 
+            this.selectSortBtn.Location = new System.Drawing.Point(12, 219);
+            this.selectSortBtn.Name = "selectSortBtn";
+            this.selectSortBtn.Size = new System.Drawing.Size(120, 63);
+            this.selectSortBtn.TabIndex = 4;
+            this.selectSortBtn.Text = "Selection Sort!";
+            this.selectSortBtn.UseVisualStyleBackColor = true;
+            this.selectSortBtn.Click += new System.EventHandler(this.selectSortBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 498);
+            this.Controls.Add(this.selectSortBtn);
+            this.Controls.Add(this.shuffleArrayBtn);
+            this.Controls.Add(this.drawArray);
+            this.Controls.Add(this.insertSortBtn);
+            this.Controls.Add(this.drawingArea);
+            this.Name = "Form1";
+            this.Text = "Sorting Visualization";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.drawingArea)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawingArea;
+        private System.Windows.Forms.Button insertSortBtn;
+        private System.Windows.Forms.Button drawArray;
+        private System.Windows.Forms.Button shuffleArrayBtn;
+        private System.Windows.Forms.Button selectSortBtn;
+    }
+}
+
---
+++ ArrayVisualization/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayVisualization
+{
+    public partial class Form1 : Form
+    {
+
+        Graphics drawArea;
+        const int arrSize = 40;
+        int[] arr = new int[arrSize];
+
+        public Form1()
+        {
+            InitializeComponent();
+            drawArea = drawingArea.CreateGraphics();
+            arr = fillArray(arr);
+        }
+
+        private void drawArray_Click(object sender, EventArgs e)
+        {
+            int drawHeight = drawingArea.Height;
+            int drawWidth = drawingArea.Width;
+            int padding = 0;
+
+            drawArea.Clear(drawingArea.BackColor);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Rectangle rectDraw = new Rectangle((i + padding), drawHeight - arr[i], 10, drawHeight);
+                drawArea.FillRectangle(Brushes.Black, rectDraw);
+                padding += (drawWidth / arrSize);
+            }
+        }
+
+        private void shuffleArrayBtn_Click(object sender, EventArgs e)
+        {
+            drawArea.Clear(drawingArea.BackColor);
+            arr = shuffleArray(arr);
+            int drawHeight = drawingArea.Height;
+            int drawWidth = drawingArea.Width;
+            int padding = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Rectangle rectDraw = new Rectangle((i + padding), drawHeight - arr[i], 10, drawHeight);
+                drawArea.FillRectangle(Brushes.Black, rectDraw);
+                padding += (drawWidth / arrSize);
+            }
+
+        }
+
+        private void insertSortBtn_Click(object sender, EventArgs e)
+        {
+            drawArea.Clear(drawingArea.BackColor);
+            insertionSort(ref arr);
+            int drawHeight = drawingArea.Height;
+            int drawWidth = drawingArea.Width;
+            int padding = 0;
+
+            drawArea.Clear(drawingArea.BackColor);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Rectangle rectDraw = new Rectangle((i + padding), drawHeight - arr[i], 10, drawHeight);
+                drawArea.FillRectangle(Brushes.Black, rectDraw);
+                padding += (drawWidth / arrSize);
+            }
+            
+        }
+
+        private void selectSortBtn_Click(object sender, EventArgs e)
+        {
+            drawArea.Clear(drawingArea.BackColor);
+            selectionSort(ref arr);
+            int drawHeight = drawingArea.Height;
+            int drawWidth = drawingArea.Width;
+            int padding = 0;
+
+            drawArea.Clear(drawingArea.BackColor);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Rectangle rectDraw = new Rectangle((i + padding), drawHeight - arr[i], 10, drawHeight);
+                drawArea.FillRectangle(Brushes.Black, rectDraw);
+                padding += (drawWidth / arrSize);
+            }
+        }
+
+        private int[] fillArray(int[] array)
+        {
+            int extraLength = 0;
+            for(int i = 1; i < array.Length+1; i++)
+            {
+                extraLength += 10;
+                array[i-1] = i+extraLength;
+            }
+            return array;
+        }
+
+        private int[] shuffleArray(int[] array)
+        {
+            Random r = new Random();
+            for (int i = array.Length; i > 0; i--)
+            {
+                int j = r.Next(i);
+                int k = array[j];
+                array[j] = array[i - 1];
+                array[i - 1] = k;
+            }
+            return array;
+        }
+
+        private void insertionSort(ref int[] array)
+        {
+            int drawHeight = drawingArea.Height;
+            int drawWidth = drawingArea.Width;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int padding = 0;
+
+                int x = array[i];
+                int j = i - 1;
+                while (j >= 0 && array[j] > x)
+                {
+                    int padding2 = 0;
+                    for (int k = 0; k < arr.Length; k++)
+                    {
+                        if (k == j || k == i)
+                        {
+                            Rectangle rectDraw = new Rectangle((k + padding2), drawHeight - arr[k], 10, drawHeight);
+                            drawArea.FillRectangle(Brushes.Red, rectDraw);
+                            padding2 += (drawWidth / arrSize);
+                        }
+                        else
+                        {
+                            Rectangle rectDraw = new Rectangle((k + padding2), drawHeight - arr[k], 10, drawHeight);
+                            drawArea.FillRectangle(Brushes.Black, rectDraw);
+                            padding2 += (drawWidth / arrSize);
+                        }
+                    }
+                    System.Threading.Thread.Sleep(25);
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                drawArea.Clear(drawingArea.BackColor);
+                for (int k = 0; k < arr.Length; k++)
+                {
+                    if (k == i)
+                    {
+                        Rectangle rectDraw = new Rectangle((k + padding), drawHeight - arr[k], 10, drawHeight);
+                        drawArea.FillRectangle(Brushes.Red, rectDraw);
+                    }
+                    else
+                    {
+                        Rectangle rectDraw = new Rectangle((k + padding), drawHeight - arr[k], 10, drawHeight);
+                        drawArea.FillRectangle(Brushes.Black, rectDraw);
+
+                    }
+                    padding += (drawWidth / arrSize);
+                }
+                array[j + 1] = x;
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+        private void selectionSort(ref int[] arr)
+        {
+            int drawHeight = drawingArea.Height;
+            int drawWidth = drawingArea.Width;
+
+            int i, j;
+            for(j = 0; j < arr.Length; j++)
+            {
+                int padding = 0;
+                int iMin = j;
+                for(i = j+1; i < arr.Length; i++)
+                {
+                    if(arr[i] < arr[iMin])
+                    {
+                        iMin = i;
+                    }
+                }
+                if(iMin != j)
+                {
+                    drawArea.Clear(drawingArea.BackColor);
+                    for (int k = 0; k < arr.Length; k++)
+                    {
+                        if (k == iMin || k == j)
+                        {
+                            Rectangle rectDraw = new Rectangle((k + padding), drawHeight - arr[k], 10, drawHeight);
+                            drawArea.FillRectangle(Brushes.Red, rectDraw);
+                        }
+                        else
+                        {
+                            Rectangle rectDraw = new Rectangle((k + padding), drawHeight - arr[k], 10, drawHeight);
+                            drawArea.FillRectangle(Brushes.Black, rectDraw);
+
+                        }
+                        padding += (drawWidth / arrSize);
+                    }
+                    System.Threading.Thread.Sleep(200);
+
+                    int temp = arr[iMin];
+                    arr[iMin] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
