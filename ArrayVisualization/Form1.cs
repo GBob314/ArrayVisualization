b87d9c7ad1b003402d04803da1cc123226718226
Robert Gonser <gbob314@gmail.com>
2016-05-18 23:19:40 -0700

    Added comments everywhere to maybe help understand the mess.

--- ArrayVisualization/Form1.cs
+++ ArrayVisualization/Form1.cs
@@ -1,19 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+/*
+ * These lines of code show up many times in this program.
+ * This is the basic drawing logic to draw all of the pretty rectangles
+ * that represent the values in the array.
+ * Rectangle height is tied to its value in the array.
+ * 
+            int drawHeight = drawingArea.Height;
+            int drawWidth = drawingArea.Width;
+            int padding = 0;
+
+            drawArea.Clear(drawingArea.BackColor);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Rectangle rectDraw = new Rectangle((i + padding), drawHeight - arr[i], 10, drawHeight);
+                drawArea.FillRectangle(Brushes.Black, rectDraw);
+                padding += (drawWidth / arrSize);
+            }
+ *
+ *
+*/
 
 namespace ArrayVisualization
 {
     public partial class Form1 : Form
     {
 
-        //THERE ARE NO COMMENTS HAHAHAHA!!!
+        //I LOVE GLOBALS
         Graphics drawArea;
         const int arrSize = 40;
         int[] arr = new int[arrSize];
@@ -25,6 +40,7 @@ namespace ArrayVisualization
             arr = fillArray(arr);
         }
 
+        //Just display the array on the screen, nothing fancy going on here.
         private void drawArray_Click(object sender, EventArgs e)
         {
             int drawHeight = drawingArea.Height;
@@ -40,6 +56,7 @@ namespace ArrayVisualization
             }
         }
 
+        //Calls the shuffleArray function to shuffle the array (duh) and then draws it to the screen.
         private void shuffleArrayBtn_Click(object sender, EventArgs e)
         {
             drawArea.Clear(drawingArea.BackColor);
@@ -57,6 +74,7 @@ namespace ArrayVisualization
 
         }
 
+        //Calls upon insertionSort function to sort the array and draws the final sorted array on the screen.
         private void insertSortBtn_Click(object sender, EventArgs e)
         {
             drawArea.Clear(drawingArea.BackColor);
@@ -75,6 +93,7 @@ namespace ArrayVisualization
             
         }
 
+        //Calls the selectionSort function to sort the array and then draws the array to the screen.
         private void selectSortBtn_Click(object sender, EventArgs e)
         {
             drawArea.Clear(drawingArea.BackColor);
@@ -92,6 +111,7 @@ namespace ArrayVisualization
             }
         }
 
+        //This is used to fill the array with values when the program is initialized.
         private int[] fillArray(int[] array)
         {
             int extraLength = 0;
@@ -103,6 +123,7 @@ namespace ArrayVisualization
             return array;
         }
 
+        //Shuffles the array.
         private int[] shuffleArray(int[] array)
         {
             Random r = new Random();
@@ -116,6 +137,8 @@ namespace ArrayVisualization
             return array;
         }
 
+        //General insertion sort with the basic logic "borrowed" from wikipedia.
+        //Also has the stated drawing logic from above in just the right place to help visualize everything.
         private void insertionSort(ref int[] array)
         {
             int drawHeight = drawingArea.Height;
@@ -129,6 +152,7 @@ namespace ArrayVisualization
                 int j = i - 1;
                 while (j >= 0 && array[j] > x)
                 {
+                    //Start drawing logic for outter loop....
                     int padding2 = 0;
                     for (int k = 0; k < arr.Length; k++)
                     {
@@ -146,9 +170,13 @@ namespace ArrayVisualization
                         }
                     }
                     System.Threading.Thread.Sleep(25);
+                    //End drawing logic for outter loop, and make it sleep to look nice.
+
                     array[j + 1] = array[j];
                     j = j - 1;
                 }
+
+                //Start drawing logic for inner loop...
                 drawArea.Clear(drawingArea.BackColor);
                 for (int k = 0; k < arr.Length; k++)
                 {
@@ -165,11 +193,14 @@ namespace ArrayVisualization
                     }
                     padding += (drawWidth / arrSize);
                 }
+                //End drawing logic for inner loop and sleep to make it look nice.
                 array[j + 1] = x;
                 System.Threading.Thread.Sleep(100);
             }
         }
 
+        //General selection sort with the basic logic "borrowed" from wikipedia.
+        //Also has the stated drawing logic from above in just the right place to help visualize everything.
         private void selectionSort(ref int[] arr)
         {
             int drawHeight = drawingArea.Height;
@@ -189,6 +220,7 @@ namespace ArrayVisualization
                 }
                 if(iMin != j)
                 {
+                    //HELLO DRAWING LOGIC!!
                     drawArea.Clear(drawingArea.BackColor);
                     for (int k = 0; k < arr.Length; k++)
                     {
@@ -214,6 +246,7 @@ namespace ArrayVisualization
             }
         }
 
+        //Calls upon QuickSort_Recursive function to sort the array and draws the final sorted array on the screen.
         private void quickSortBtn_Click(object sender, EventArgs e)
         {
             drawArea.Clear(drawingArea.BackColor);
@@ -231,6 +264,9 @@ namespace ArrayVisualization
             }
         }
 
+        //This quick sort and the partition were "borrowed" from a website:
+        //http://www.softwareandfinance.com/CSharp/QuickSort_Recursive.html
+        //ALSO HAS MORE DRAWING LOGIC TO MAKE IT LOOK NICE!!! 
         private int Partition(int[] numbers, int left, int right)
         {
             int drawHeight = drawingArea.Height;
@@ -296,6 +332,7 @@ namespace ArrayVisualization
             }
         }
 
+        //IDK what this does but it breaks the program if you get rid of it...
         private void Form1_Load(object sender, EventArgs e)
         {
 
